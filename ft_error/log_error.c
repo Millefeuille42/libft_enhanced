e814126dfb15cc6702b30e9d0ffeda7f011cc8ca
Millefeuille <mlabouri@student.42.fr>
2023-09-19 17:17:35 +0200

    changed ft_error to use ft_log

--- ft_error/log_error.c
+++ ft_error/log_error.c
@@ -5,18 +5,7 @@
 #include "ft_error.h"
 
 void log_error(char *add) {
-    ft_fputstr("ft_nm: error: ", 1);
-	ft_fputstr(add, 1);
-	switch (errno) {
-		case ENOENT:
-			ft_fputstr(" no such file", 1);
-			break;
-		case EINVAL:
-			ft_fputstr(" file format not recognized", 1);
-			break;
-		default:
-			ft_fputstr(" error", 1);
-			break;
-	}
-	ft_putchar('\n');
+    ft_logstr(ERROR, PROGRAM_NAME);
+	ft_logstr_no_header(ERROR, ": error: ");
+	perror(add);
 }
